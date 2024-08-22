3687e6729b68fd02662e9a622fd401be7383afba
turtle123456789 <90664619+turtle123456789@users.noreply.github.com>
2024-08-22 14:41:17 +0700

    d

--- src/pages/adminPahe.jsx
+++ src/pages/adminPahe.jsx
@@ -316,27 +316,6 @@ return (
                         </div>
                     </div>
                     <div className='flex items-center mb-2'>
-                        <p className='text-md whitespace-nowrap text-[#fff]'>Lãi suất:</p>
-                        <div className="relative z-0 w-full group ml-2">
-                            <input type="text" name='interestRate' value={formData.interestRate} onChange={handleChange} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                            <label className="peer-focus:font-medium absolute left-1 text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Lãi suất</label>
-                        </div>
-                    </div>
-                    <div className='flex items-center mb-2'>
-                        <p className='text-md whitespace-nowrap text-[#fff]'>Phí dịch vụ:</p>
-                        <div className="relative z-0 w-full group ml-2">
-                            <input type="text" name='serviceFee' value={formData.serviceFee} onChange={handleChange} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                            <label className="peer-focus:font-medium absolute left-1 text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phí dịch vụ</label>
-                        </div>
-                    </div>
-                    <div className='flex items-center mb-2'>
-                        <p className='text-md whitespace-nowrap text-[#fff]'>Miễn giảm:</p>
-                        <div className="relative z-0 w-full group ml-2">
-                            <input type="text" name='exemption' value={formData.exemption} onChange={handleChange} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                            <label className="peer-focus:font-medium absolute left-1 text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Miễn giảm</label>
-                        </div>
-                    </div>
-                    <div className='flex items-center mb-2'>
                         <p className='text-md whitespace-nowrap text-[#fff]'>Số tiền cần thanh toán:</p>
                         <div className="relative z-0 w-full group ml-2">
                             <input type="text" name='amountPaid' value={formData.amountPaid} onChange={handleChange} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,25 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const HomePage = () => {
+  const [color,setColor] = useState("")
   const user = JSON.parse(localStorage.getItem("userLogin"))
-
+  useEffect(()=>{
+    if(user.status==="đã thanh toán" || "Đã thanh toán " || "Đã Thanh Toán" || "ĐÃ THANH TOÁN" )
+      { 
+        setColor("#05ff71")
+      }else if(user.status==="chưa thanh toán" || "Chưa thanh toán " || "Chưa Thanh Toán" || "CHƯA THANH TOÁN")
+      {
+        setColor("#ff0505")
+      }else{
+        setColor("")
+      }
+  },[user.status])
+  
   return (
     <div>
-        <div className='flex items-center justify-center h-screen'>
-            <div className="w-full max-w-[1050px] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                <h1 className='text-3xl font-medium text-gray-900 dark:text-white'>Thông tin khách hàng</h1>
+        <div className='flex items-center justify-center h-screen '>
+            <div className="w-full max-w-[1050px] bg-[#ffe2c2] border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <h1 className='text-3xl font-medium text-gray-900 dark:text-white'>Thông tin khoản vay</h1>
                 <div className='p-11'>
-                    <p className="mb-1 flex text-xl font-medium text-gray-900 dark:text-white">Họ tên khách: <span className='ml-4 text-lg'>{user.name}</span></p>
-                    <p className="mb-1 flex text-xl font-medium text-gray-900 dark:text-white">Số điện thoại: <span className='ml-4 text-lg'>{user.phone}</span></p>
-                    <p className="mb-1 flex text-xl font-medium text-gray-900 dark:text-white">Số lượng khoản vay: <span className='ml-4 text-lg'>{user.debt}</span></p>
-                    <p className="mb-1 flex text-xl font-medium text-gray-900 dark:text-white">Số tiền vay: <span className='ml-4 text-lg'>{user.loan}</span></p>
-                    <p className="mb-1 flex text-xl font-medium text-gray-900 dark:text-white">Lãi suất: <span className='ml-4 text-lg'>{user.interestRate}</span></p>
-                    <p className="mb-1 flex text-xl font-medium text-gray-900 dark:text-white">Phí dịch vụ: <span className='ml-4 text-lg'>{user.serviceFee}</span></p>
-                    <p className="mb-1 flex text-xl font-medium text-gray-900 dark:text-white">Miễn giảm: <span className='ml-4 text-lg'>{user.exemption}</span></p>
-                    <p className="mb-1 flex text-xl font-medium text-gray-900 dark:text-white">Số tiền cần thanh toán: <span className='ml-4 text-lg'>{user.amountPaid}</span></p>
-                    <p className="mb-1 flex text-xl font-medium text-gray-900 dark:text-white">Ngày vay: <span className='ml-4 text-lg'>{user.status}</span></p>
-                    <p className="mb-1 flex text-xl font-medium text-gray-900 dark:text-white">Thời hạn thanh toán: <span className='ml-4 text-lg'>{user.loanDate}</span></p>
-                    <p className="mb-1 flex text-xl font-medium text-gray-900 dark:text-white">Tình trạng: <span className='ml-4 text-lg'>{user.paymentTerm}</span></p>
+                    <p className="mb-1 flex text-xl font-bold text-gray-900 dark:text-white">Họ tên khách: <span className='ml-4 font-medium text-lg'>{user.name}</span></p>
+                    <p className="mb-1 flex text-xl font-bold text-gray-900 dark:text-white">Số điện thoại: <span className='ml-4 font-medium text-lg'>{user.phone}</span></p>
+                    <p className="mb-1 flex text-xl font-bold text-gray-900 dark:text-white">Số lượng khoản vay: <span className='ml-4 font-medium text-lg'>{user.debt}</span></p>
+                    <p className="mb-1 flex text-xl font-bold text-gray-900 dark:text-white">Số tiền vay: <span className='ml-4 font-medium text-lg'>{user.loan}</span></p>
+                    <p className="mb-1 flex text-xl font-bold text-gray-900 dark:text-white">Số tiền cần thanh toán: <span className='ml-4 font-medium text-lg'>{user.amountPaid}</span></p>
+                    <p className="mb-1 flex text-xl font-bold text-gray-900 dark:text-white">Ngày vay: <span className={`ml-4 font-medium text-lg`} style={{ color }}>{user.status}</span></p>
+                    <p className="mb-1 flex text-xl font-bold text-gray-900 dark:text-white">Thời hạn thanh toán: <span className='ml-4 font-medium text-lg'>{user.loanDate}</span></p>
+                    <p className="mb-1 flex text-xl font-bold text-gray-900 dark:text-white">Tình trạng: <span className='ml-4 font-medium text-lg'>{user.paymentTerm}</span></p>
                 </div>
             </div>
         </div>
