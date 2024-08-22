e68ed2e23b0d78fd3261a01047028173d2dc3e0b
turtle123456789 <90664619+turtle123456789@users.noreply.github.com>
2024-08-22 13:31:12 +0700

    d

--- src/components/headerComponent.jsx
+++ src/components/headerComponent.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-
+import logo from "../assets/imgs/logo.png"
 const HeaderComponent = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const user = JSON.parse(localStorage.getItem("userLogin"))
@@ -11,8 +11,8 @@ const HeaderComponent = () => {
     <nav className="bg-white border-gray-200 dark:bg-[#374151]">
         <div className="flex flex-wrap items-center justify-between max-w-screen-xl mx-auto p-4">
             <Link to="/home" className="flex items-center space-x-3 rtl:space-x-reverse">
-                <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Management</span>
+                <img src={logo} className="h-8" alt="Flowbite Logo" />
+                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Moneyvay</span>
             </Link>
             <div  className="flex items-center md:order-2 space-x-1 md:space-x-2 rtl:space-x-reverse">
                 <Link to="/login" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Đăng xuất</Link>
